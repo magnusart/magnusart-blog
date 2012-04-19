36a67367b1fb9e1dfdd4408755768ca3f6cf1a9a
Magnus Andersson <magnus@magnusart.com>
2012-04-19 11:26:12 +0200

    Adding Twitter Bootstrap

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,21 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Magnusart"
-    val appVersion      = "1.0-SNAPSHOT"
+	val appName         = "Magnusart"
+	val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
+	val appDependencies = Seq(
       // Add your project dependencies here,
-    )
+      )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+    // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
+    def customLessEntryPoints(base: File): PathFinder = ( 
+    	(base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++ 
+    	(base / "app" / "assets" / "stylesheets" * "*.less") )
+
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( 
+    	lessEntryPoints <<= baseDirectory(customLessEntryPoints) 
+    	)
 
 }
