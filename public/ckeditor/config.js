13f8405a9f8fc13d5cf1f8bcf160134d527bd7ea
Magnus Andersson <magnus@magnusart.com>
2012-04-25 23:51:01 +0200

    Replaced WYSIHTML5 with CKEditor + Bootstrap theme.

---
+++ public/ckeditor/config.js
@@ -0,0 +1,19 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+
+	config.toolbar = [{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+	'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+	{ name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] }];
+	config.skin = 'BootstrapCK-Skin';
+	config.toolbarStartupExpanded = true;
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
